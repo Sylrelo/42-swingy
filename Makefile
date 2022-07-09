86c790d52d8345e04efbf305b921598c7e4663e8
Sylrelo <synlo@outlook.com>
2022-07-09 22:09:48 +0200

    Init

---
+++ Makefile
@@ -0,0 +1,3 @@
+run:
+	mvn package
+	java -jar target/swingy-1.0-SNAPSHOT.jar
---
+++ src/main/java/com/slopez/swingy/Main.java
@@ -0,0 +1,9 @@
+package com.slopez.swingy;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
